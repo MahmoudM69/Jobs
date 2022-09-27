b0d6e471b1582487436596dfe4fb9372cfa7c539
anas <anas@DESKTOP-AOG59KC>
2022-09-28 01:19:31 +0200

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Jobs.Models;
+
+namespace Jobs.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Jobs.Models.Category> Category { get; set; }
+        public DbSet<Jobs.Models.Job> Job { get; set; }
+    }
+}
---
+++ Models/Job.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace Jobs.Models
+{
+    public class Job
+    {
+        public int Id { get; set; }
+        [DisplayName("إسم الوظيفة")]
+        public string Title { get; set; }
+        [DisplayName("وصف الوظيفة")]
+        public string Content { get; set; }
+        [DisplayName("صورة الوظيفة")]
+        public IFormFile Image { get; set; }
+        [DisplayName("نوع الوظيفة")]
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
